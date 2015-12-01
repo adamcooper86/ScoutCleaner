57e537108afa8883b0a5ea35bcf57eba9bbaa0aa
Adam Fluke <fluke.adam@gmail.com>
2015-12-01 10:55:42 -0800

    Add controller logic to allow for path to be passed in as argv
    -also adds simple benchmarking

--- scoutcleaner.rb
+++ scoutcleaner.rb
@@ -2,8 +2,23 @@ require_relative 'cleaner'
 require_relative 'scout_view'
 
 class ScoutCleanerController
-  def initialize
+  def initialize path_info
+    @view = ScoutView.new
+    if path_info
+      path = path_info
+    else
+      path = @view.get_path
+    end
+    @view.message(Cleaner.clean_folder path)
+    @view.goodbye
   end
 end
 
-ScoutCleanerController.new
+path_info = ARGV[0]
+
+start = Time.now
+ScoutCleanerController.new path_info
+finish = Time.now
+
+puts "Time for 100 renames: "
+puts finish - start
