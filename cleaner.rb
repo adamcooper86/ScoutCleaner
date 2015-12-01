f3566e63886df2b3e37aa757620e7b97507392db
Adam Fluke <fluke.adam@gmail.com>
2015-12-01 09:55:54 -0800

    Break out logic for renaming a file into another method

--- cleaner.rb
+++ cleaner.rb
@@ -2,14 +2,17 @@ class Cleaner
   def self.clean_folder path
     Dir.glob(path + "/**").sort.each do |file|
       if File.file?(file)
-        filename = self.clean_file_name file
-        filename = path + "/" + filename
-        File.rename file, filename
+        self.rename_file file, path
       else
         puts file + " is a folder"
       end
     end
     "Cleaned files in folder: " + path
+  end
+  def self.rename_file file, path
+    filename = self.clean_file_name file
+    filename = path + "/" + filename
+    File.rename file, filename
   end
   def self.clean_file_name file
     extension = File.extname file
