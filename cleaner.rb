c1133ecba37176aa7430fdfa5fe1d91cc0b02048
Adam Fluke <fluke.adam@gmail.com>
2015-12-01 08:57:02 -0800

    Create cleanser class methods to rename files

--- cleaner.rb
+++ cleaner.rb
@@ -1,2 +1,13 @@
 class Cleaner
+  def self.clean_folder path
+  end
+  def self.clean_file_name file
+    extension = File.extname file
+    base_name = File.basename file, extension
+    base_name = self.clean_string base_name
+    base_name + extension
+  end
+  def self.clean_string string = ""
+    string.gsub /[^\w,\-]/, "_"
+  end
 end
--- spec/cleaner_spec.rb
+++ spec/cleaner_spec.rb
@@ -1,7 +1,58 @@
 require 'spec_helper'
 
 describe Cleaner do
-  it 'says hi' do
-    expect(Cleaner.hi).to eq "hi"
+  context ".clean_string" do
+    it 'returns an empty string when passed no arguement' do
+      expect(Cleaner.clean_string).to eq ""
+    end
+    it 'returns a the same string when no special characters are present' do
+      expect(Cleaner.clean_string "apple").to eq "apple"
+      expect(Cleaner.clean_string "apple_sause").to eq "apple_sause"
+    end
+    it 'returns a string with spaces replaced' do
+      expect(Cleaner.clean_string "apple sause").to eq "apple_sause"
+      expect(Cleaner.clean_string "apple  sause").to eq "apple__sause"
+      expect(Cleaner.clean_string 'ap\ple sause').to eq "ap_ple_sause"
+      expect(Cleaner.clean_string "apple/ sause").to eq "apple__sause"
+      expect(Cleaner.clean_string "apple:sause").to eq "apple_sause"
+      expect(Cleaner.clean_string "apple*sause").to eq "apple_sause"
+      expect(Cleaner.clean_string "apple?sause").to eq "apple_sause"
+      expect(Cleaner.clean_string 'apple"sause').to eq "apple_sause"
+      expect(Cleaner.clean_string "apple<sause").to eq "apple_sause"
+      expect(Cleaner.clean_string "apple>sause").to eq "apple_sause"
+      expect(Cleaner.clean_string "apple|sause").to eq "apple_sause"
+      expect(Cleaner.clean_string "apple#sause").to eq "apple_sause"
+      expect(Cleaner.clean_string "apple%sause").to eq "apple_sause"
+      expect(Cleaner.clean_string "apple~sause").to eq "apple_sause"
+      expect(Cleaner.clean_string "apple&sause").to eq "apple_sause"
+      expect(Cleaner.clean_string "apple}sause").to eq "apple_sause"
+      expect(Cleaner.clean_string "apple;sause").to eq "apple_sause"
+      expect(Cleaner.clean_string "apple{sause").to eq "apple_sause"
+    end
+  end
+  context ".clean_file_name" do
+    it 'returns the same file if not given a valid file' do
+      expect(Cleaner.clean_file_name "hello.txt").to eq "hello.txt"
+    end
+    it 'returns a valid file name if given an invalid string' do
+      expect(Cleaner.clean_file_name "hello hello.txt").to eq "hello_hello.txt"
+      expect(Cleaner.clean_file_name "hello  hello.txt").to eq "hello__hello.txt"
+      expect(Cleaner.clean_file_name 'hello\hello.txt').to eq "hello_hello.txt"
+      expect(Cleaner.clean_file_name "hello/hello.txt").to eq "hello.txt"
+      expect(Cleaner.clean_file_name "hello:hello.txt").to eq "hello_hello.txt"
+      expect(Cleaner.clean_file_name "hello*hello.txt").to eq "hello_hello.txt"
+      expect(Cleaner.clean_file_name "hello?hello.txt").to eq "hello_hello.txt"
+      expect(Cleaner.clean_file_name 'hello"hello.txt').to eq "hello_hello.txt"
+      expect(Cleaner.clean_file_name "hello<hello.txt").to eq "hello_hello.txt"
+      expect(Cleaner.clean_file_name "hello>hello.txt").to eq "hello_hello.txt"
+      expect(Cleaner.clean_file_name "hello|hello.txt").to eq "hello_hello.txt"
+      expect(Cleaner.clean_file_name "hello#hello.txt").to eq "hello_hello.txt"
+      expect(Cleaner.clean_file_name "hello%hello.txt").to eq "hello_hello.txt"
+      expect(Cleaner.clean_file_name "hello~hello.txt").to eq "hello_hello.txt"
+      expect(Cleaner.clean_file_name "hello&hello.txt").to eq "hello_hello.txt"
+      expect(Cleaner.clean_file_name "hello}hello.txt").to eq "hello_hello.txt"
+      expect(Cleaner.clean_file_name "hello{hello.txt").to eq "hello_hello.txt"
+      expect(Cleaner.clean_file_name "hello;hello.txt").to eq "hello_hello.txt"
+    end
   end
 end
